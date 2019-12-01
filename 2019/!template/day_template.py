9b40a757ae09717d3b04b762488671c515ea29f3
n3rden <aiden@vaines.org>
2019-12-01 12:51:01 +0000

    adding template

---
+++ 2019/!template/day_template.py
@@ -0,0 +1,19 @@
+"""
+task summary/notes
+""" 
+
+import fileinput
+
+puzzle_input = list(fileinput.input('sample1.txt'))
+
+
+def part1():
+    return "part 1"
+
+
+def part2():
+    return "part 2"
+
+
+print(part1())
+print(part2())
