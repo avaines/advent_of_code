f3cf230f746d1c528273b173b43762b6a4a9749b
Aiden Vaines <aiden@vaines.org>
2020-12-03 09:52:07 +0000

    refactor of logic + part 2

--- 2020/day3/main.py
+++ 2020/day3/main.py
@@ -5,10 +5,16 @@ task summary/notes
 import fileinput
 import collections
 
-def part1(puzzle_input):
+def part1(puzzle_input, draw):
+
+    return slope_master(puzzle_input, 3, 1, draw)
+
+
+def slope_master(puzzle_input, route_right, route_down, draw):
     pos_x = 0 # _
     pos_y = 0 # |
     tree_count = 0
+    route_down_check = route_down
 
     cur_y_row = 0
     while(pos_y < len(puzzle_input) ):
@@ -17,7 +23,7 @@ def part1(puzzle_input):
             #This position IS the x column of the current row
             if col == pos_x and cur_y_row == pos_y:
                 if puzzle_input[pos_y][col] == "#":
-                    #This postion is a tree at yx 
+                    #This postion is a tree at yx
                     print("X", end='', sep='')
                     tree_count +=1
                 else:
@@ -32,8 +38,13 @@ def part1(puzzle_input):
         print("")
 
         # Increment all the things
-        pos_y +=1
-        pos_x +=3
+        if route_down_check == 1:
+            pos_y += route_down
+            pos_x += route_right
+            route_down_check = route_down
+        else:
+            route_down_check = route_down_check-1
+
         cur_y_row +=1
 
         # If x is wider than then width of the map, strip the length of the visible display and start again
@@ -44,18 +55,25 @@ def part1(puzzle_input):
     return tree_count
 
 
+def part2(puzzle_input, draw):
+    # List of routes (right, down)
+    routes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
-def part2(puzzle_input):
+    tree_count_product = 1
 
-    return 0
+    for route in routes:
+        print("Right: ", route[0], ", Down: ", route[1])
+        tree_count_product = tree_count_product * slope_master(puzzle_input, route[0], route[1], draw)
+
+    return tree_count_product
 
 
 # puzzle_input = list(fileinput.input('sample1.txt'))
 with open('input.txt', 'r') as myfile:
     puzzle_input = myfile.read().split("\n")
 
-p1 = part1(puzzle_input)
-p2 = part2(puzzle_input)
+p1 = part1(puzzle_input, False)
+p2 = part2(puzzle_input, False)
 
 print("#############################################################")
 print("Part1: ", p1 )
