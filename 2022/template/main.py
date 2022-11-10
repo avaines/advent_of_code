78fe32cec41b309e2bccdccd3e6864ed54e29a1e
n3rden <aiden@vaines.org>
2022-11-10 21:45:41 +0000

    2022 AOC Template

---
+++ 2022/template/main.py
@@ -0,0 +1,38 @@
+"""
+Day X: Title
+"""
+INPUT_DEBUG = True
+P1_DEBUG    = True
+P2_DEBUG    = True
+
+USE_REAL_DATA = False # Loads input.txt when True or sample.txt when False
+
+INPUT_FILENAME  = "./input.txt"
+SAMPLE_FILENAME = "./sample.txt"
+
+
+def input_parser(input):
+    if INPUT_DEBUG: print("Parsing the input")
+    return input
+
+def part1(input):
+    if P1_DEBUG: print("Doing Part 1 things")
+    return "part 1 answer"
+
+def part2(input):
+    if P2_DEBUG: print("Doing Part 2 things")
+    return "part 2 answer"
+
+
+if __name__ == '__main__':
+    with open(INPUT_FILENAME if USE_REAL_DATA else SAMPLE_FILENAME, 'r') as input_file:
+        parsed_input = input_parser(input_file.read().split("\n"))
+
+    part_1 = part1(parsed_input)
+
+    part_2 = part2(parsed_input)
+
+    print()
+    print("# # # SOLUTIONS # # #")
+    print("Part1:", part_1 )
+    print("Part2:", part_2 )
