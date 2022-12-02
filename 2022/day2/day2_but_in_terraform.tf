b68377552cdd6ce71014a279df95aff5a474d0e2
n3rden <aiden@vaines.org>
2022-12-02 14:58:09 +0000

    day2 part 2 in TF

--- 2022/day2/day2_but_in_terraform.tf
+++ 2022/day2/day2_but_in_terraform.tf
@@ -1,11 +1,11 @@
 locals {
-    input = file("sample.txt")
+    input = file("input.txt")
     processed_input = split("\n", local.input)
 
     score_lookup = {
         "A X" = 1+3
         "A Y" = 2+6
-        "A Z" = 3+3
+        "A Z" = 3+0
         "B X" = 1+0
         "B Y" = 2+3
         "B Z" = 3+6
@@ -14,11 +14,28 @@ locals {
         "C Z" = 3+3
     }
 
-    the_game = sum([ for round in local.processed_input: local.score_lookup[round]])
+/*
+A B C is still R P S
+X Y Z is now must lose, draw, win
+*/
+    score_calculated_lookup = {
+        "A X" = local.score_lookup["A Z"] # lose
+        "A Y" = local.score_lookup["A X"] # draw
+        "A Z" = local.score_lookup["A Y"] # win
+        "B X" = local.score_lookup["B X"] # lose
+        "B Y" = local.score_lookup["B Y"] # draw
+        "B Z" = local.score_lookup["B Z"] # win
+        "C X" = local.score_lookup["C Y"] # lose
+        "C Y" = local.score_lookup["C Z"] # draw
+        "C Z" = local.score_lookup["C X"] # win
+    }
 
 }
 
 output "part1" {
-    value = local.the_game
+    value = sum([ for round in local.processed_input: local.score_lookup[round]])
 }
 
+output "part2" {
+    value = sum([ for round in local.processed_input: local.score_calculated_lookup[round]])
+}
