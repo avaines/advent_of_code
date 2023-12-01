fe2d83f67fc9a2c1329c72cd5934204b94bb2c75
avaines <aiden@vaines.org>
2023-12-01 09:31:09 +0000

    Adding 2023 day 1

---
+++ 2023/day1/main.py
@@ -0,0 +1,64 @@
+
+# Import AOC Common
+import os, sys
+sys.path.append("../")
+from shared import aoc_common, aoc_algorithms
+import regex
+
+P1_DEBUG    = True
+P2_DEBUG    = True
+
+USE_REAL_DATA = True # Loads input.txt when True or sample.txt when False
+
+INPUT_FILENAME  = "%s/input.txt" % os.path.dirname(os.path.realpath(__file__))
+SAMPLE_FILENAME = "%s/sample.txt" % os.path.dirname(os.path.realpath(__file__))
+
+def part1(input):
+    raw_instructions = []
+    for line in input:
+        digits = [int(s) for s in [*line] if s.isdigit()]
+        raw_instructions.append( f"{digits[0]}{digits[-1]}")
+
+    return sum(list(map(int, raw_instructions)))
+
+
+def part2(input):
+    raw_instructions = []
+    for line in input:
+        line_words = regex.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
+        for i, entry in enumerate(line_words):
+            match entry:
+                case "one":
+                    line_words[i] = "1"
+                case "two":
+                    line_words[i] = "2"
+                case "three":
+                    line_words[i] = "3"
+                case "four":
+                    line_words[i] = "4"
+                case "five":
+                    line_words[i] = "5"
+                case "six":
+                    line_words[i] = "6"
+                case "seven":
+                    line_words[i] = "7"
+                case "eight":
+                    line_words[i] = "8"
+                case "nine":
+                    line_words[i] = "9"
+
+        digits = [int(s) for s in [*line_words] if s.isdigit()]
+        raw_instructions.append( f"{digits[0]}{digits[-1]}")
+
+    return sum(list(map(int, raw_instructions)))
+
+
+if __name__ == '__main__':
+    parsed_input = aoc_common.import_file_single_new_line(INPUT_FILENAME if USE_REAL_DATA else SAMPLE_FILENAME)
+
+    part_1 = part1(parsed_input)
+    part_2 = part2(parsed_input)
+
+    print("# # # SOLUTIONS # # #")
+    print("Part1:", part_1 )
+    print("Part2:", part_2 )
